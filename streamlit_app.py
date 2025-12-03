71a5974a94f5c0dd14d3b37ea692a3a531abd6a3
michaellawrence089-maker <michaellawrence089@gmail.com>
2025-12-03 12:03:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,12 +125,8 @@ st.set_page_config(
     layout="wide",
 )
 
-with st.container():
-    center = st.columns(3)[1]
-    with center:
-        st.image("C:/Users/Michael/Documents/kalkubor.jpg", width=300)
-st.title("Program Perhitungan Biaya Operasional Pengeboran Batubara")
-st.caption("Program by: Kelompok 7 Berpikir Komputasional K-28")
+st.title("💼 Program Perhitungan Biaya Operasional Pengeboran Batubara")
+st.caption("Versi Streamlit - Program by: Tim Pengembangan KalkuBor")
 
 # INPUT IDENTITAS PENGGUNA
 col_nama, col_info = st.columns([2, 3])
